99d1a7cae446c98ea865dba19964bf88b58d51d5
candrawijayanto <67464295+candrawijayanto@users.noreply.github.com>
2020-07-15 18:17:25 +0800

    menambahkan fungsi zoom in zoom out

--- main.cpp
+++ main.cpp
@@ -1,234 +1,235 @@
-#include <stdlib.h>
-#include <math.h>
+#include<Windows.h>
 #include <GL/glut.h>
 
-void init(void);
-void tampil(void);
-void keyboard(unsigned char, int, int);
-void ukuran(int, int);
-void mouse(int button, int state, int x, int y);
-void mouseMotion(int x, int y);
-
-float xrot = 0.0;
-float yrot = 0.0;
-float xdiff = 0.0;
-float ydiff = 0.0;
 bool mouseDown = false;
-int is_depth;
 
-int main (int argc, char **argv)
+float xrot = 0.0f;
+float yrot = 0.0f;
+
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+
+float scale = 1.0f;
+float xmovement = 0.0f;
+float ymovement = 0.0f;
+float zmovement = 0.0f;
+
+class myObject {
+public:
+	void drawing() {
+		
+		glPointSize(2);
+		glBegin(GL_POINT);
+			glVertex3i(0,0,0);
+		glEnd();
+	}
+};
+
+void drawBox()
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(250, 80);
-    glutCreateWindow("3D");
-    init();
-	glutDisplayFunc(tampil);
-    glutKeyboardFunc(keyboard);
-    glutReshapeFunc(ukuran);
-    glutMouseFunc(mouse);
-    glutMotionFunc(mouseMotion);
-    glutMainLoop();
-    return 0;
+
+	// Base
+	glBegin(GL_TRIANGLES);
+	glColor3f(1, 1, 0);
+	glVertex3f(-0.2, -0.2, -0.2);
+	glVertex3f(-0.2, 0.2, -0.2);
+	glVertex3f(-0.2, 0.2, 0.2);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1, 1, 0);
+	glVertex3f(0.8, -0.2, -0.2);
+	glVertex3f(0.8, 0.2, -0.2);
+	glVertex3f(0.8, 0.2, 0.2);
+	glEnd();
+
+	// Red quads
+
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.2, 0.2, 0.2);
+	glVertex3f(0.8, 0.2, 0.2);
+	glVertex3f(0.8, -0.2, -0.2);
+	glVertex3f(-0.2, -0.2, -0.2);
+	glEnd();
+
+	// Green quads
+
+	glBegin(GL_QUADS);
+	glColor3f(0, 1, 0);
+	glVertex3f(-0.2, 0.2, 0.2);
+	glVertex3f(0.8, 0.2, 0.2);
+	glVertex3f(0.8, 0.2, -0.2);
+	glVertex3f(-0.2, 0.2, -0.2);
+	glEnd();
+
+	// Red quads
+
+	glBegin(GL_QUADS);
+	glColor3f(0, 0, 1);
+	glVertex3f(-0.2, 0.2, -0.2);
+	glVertex3f(0.8, 0.2, -0.2);
+	glVertex3f(0.8, -0.2, -0.2);
+	glVertex3f(-0.2, -0.2, -0.2);
+	glEnd();
+
 }
 
-void init(void)
+bool init()
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_DEPTH_TEST);
+	glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
 
-    is_depth = 1;
-    glMatrixMode(GL_MODELVIEW);
-    glPointSize(9.0);
-    glLineWidth(6.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClearDepth(1.0f);
 
+	return true;
 }
 
-void tampil(void)
+void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glRotatef(xrot,1,0,0);
-	glRotatef(yrot, 0,1,0);
-    glPushMatrix();
 
-	//depan
-	glBegin(GL_QUADS);
-	glColor3f(1.0,1.0,1.0);
-	glVertex3f(-10.0,-10.0,10.0);
-	glVertex3f(-10.0, 10.0, 10.0);
-	glVertex3f(10.0, 10.0, 10.0);
-    glVertex3f(10.0,-10.0,10.0);
-	glEnd();
+	gluLookAt(
+		0.0f, 0.0f, 3.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
 
+	glRotatef(xrot, 1.0f, 0.0f, 0.0f); // Rotating horizontal
+	glRotatef(yrot, 0.0f, 1.0f, 0.0f); // Rotating vertical
+	glScalef(scale, scale, scale); // Scalling
+	glTranslatef(xmovement, ymovement, zmovement); // Translate / Movement
 
-	//belakang
-	glBegin(GL_QUADS);
-	glColor3f(1.0,1.0,0.0);
-	glVertex3f(-10.0,-10.0, -10.0);
-	glVertex3f(-10.0, 10.0, -10.0);
-	glVertex3f(10.0, 10.0, -10.0);
-    glVertex3f(10.0,-10.0, -10.0);
-	glEnd();
+	drawBox();
 
-	//kanan
-	glBegin(GL_QUADS);
-	glColor3f(0.0,1.0,1.0);
-	glVertex3f(10.0, 10.0, 10.0);
-    glVertex3f(10.0,-10.0,10.0);
-	glVertex3f(10.0,-10.0,-10.0);
-	glVertex3f(10.0,10.0,-10.0);
-	glEnd();
-
-
-	//kiri
-	glBegin(GL_QUADS);
-	glColor3f(1.0,0.0,1.0);
-	glVertex3f(-10, 10.0, 10.0);
-	glVertex3f(-10,-10.0,10.0);
-	glVertex3f(-10,-10.0,-10.0);
-	glVertex3f(-10,10.0,-10.0);
-	glEnd();
-
-	//atas
-	glBegin(GL_QUADS);
-	glColor3f(0.0,0.5,1.0);
-	glVertex3f(-10, 10.0+10.0,10.0);
-	glVertex3f(10,-10.0+20.0,10.0);
-	glVertex3f(10,-10.0+20.0,-10.0);
-	glVertex3f(-10,10.0+10.0,-10.0);
-	glEnd();
-
-	//bawah
-	glBegin(GL_QUADS);
-	glColor3f(0.0,1.5,0.0);
-	glVertex3f(-10, -10.0, 10.0);
-	glVertex3f(10,-10.0,10.0);
-	glVertex3f(10,-10.0,-10.0);
-	glVertex3f(-10,-10.0,-10.0);
-	glEnd();
-	glPopMatrix();
-
-    // glBegin(GL_POLYGON);
-    // glColor3d(160, 70, 53);
-    // glVertex3f();
-// 
-    // glEnd();
-
-    glutSwapBuffers();
-
+	glFlush();
+	glutSwapBuffers();
 }
 
-void idle(){
-    if(!mouseDown){
-        xrot += 0.3;
-        yrot += 0.4;
-    }
-    glutPostRedisplay();
+void resize(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glViewport(0, 0, w, h);
+
+	gluPerspective(45.0f, 1.0f * w / h, 1.0f, 100.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
-void mouse(int button, int state, int x, int y){
-    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
-        mouseDown = true;
-        xdiff = x - yrot;
-        ydiff = -y + xrot;
-    }
-    else{
-        mouseDown = false;
-    }
-}
 
-void mouseMotion(int x, int y){
-    if(mouseDown){
-        yrot = x - xdiff;
-        xrot = y + ydiff;
-        glutPostRedisplay();
-    }
+void idle()
+{
+	if (!mouseDown)
+	{
+		xrot += 0.3f;
+		yrot += 0.4f;
+	}
+
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-switch (key)
-    {
+	switch (key) {
+	case 'a':
+		yrot += 5.0f;
+		break;
+	case 'd':
+		yrot -= 5.0f;
+		break;
 	case 'w':
-    case 'W':
-        xrot -= 1;
-        glTranslatef(0.0, 0.0, xrot);
-        break;
-    case 'd':
-    case 'D':
-        yrot += 1;
-        glTranslatef(yrot, 0.0, 0.0);
-        break;
+		xrot += 5.0f;
+		break;
 	case 's':
-    case 'S':
-        xrot += 1;
-        glTranslatef(0.0, 0.0, xrot);
-        break;
-    case 'a':
-    case 'A':
-        yrot -= 1;
-        glTranslatef(yrot, 0.0, 0.0);
-        break;
-    /*
-	case '7':
-        glTranslatef(0.0, 3.0, 0.0);
-        break;
-	case '9':
-        glTranslatef(0.0, -3.0, 0.0);
-        break;
-    */
-    case '2':
-        xrot += 1;
-        glRotatef(xrot, 1.0, 0.0, 0.0);
-        break;
-    case '8':
-        xrot -= 1;
-        glRotatef(xrot, 1.0, 0.0, 0.0);
-        break;
-    case '6':
-        yrot += 1;
-        glRotatef(yrot, 0.0, 1.0, 0.0);
-        break;
-    case '4':
-        yrot -= 1;
-        glRotatef(yrot, 0.0, 1.0, 0.0);
-        break;
-    /*case '1':
-        glRotatef(2.0, 0.0, 0.0, 1.0);
-        break;
-    case '3':
-        glRotatef(-2.0, 0.0, 0.0, 1.0);
-        break;
-    */
-    case '5':
-        if (is_depth)
-        {
-            is_depth = 0;
-            glDisable(GL_DEPTH_TEST);
-        }
-        else
-        {
-            is_depth = 1;
-            glEnable(GL_DEPTH_TEST);
-        }
-    }
-    tampil();
+		xrot -= 5.0f;
+		break;
+	case '1':
+		scale += 0.2f;
+		break;
+	case '2':
+		scale -= 0.2f;
+		break;
+	case 27:
+		exit(1);
+		break;
+	}
+
+	glutPostRedisplay();
 }
 
-void ukuran(int lebar, int tinggi)
+void specialKeyboard(int key, int x, int y)
 {
-    if (tinggi == 0) tinggi = 1;
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(50.0, lebar / tinggi, 5.0, 500.0);
-    glTranslatef(0.0, -5.0, -150.0);
-    glMatrixMode(GL_MODELVIEW);
+	switch (key) {
+	case 100:
+		xmovement -= 0.025f;
+		break;
+	case 101:
+		ymovement += 0.025f;
+		break;
+	case 102:
+		xmovement += 0.025f;
+		break;
+	case 103:
+		ymovement -= 0.025f;
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouseDown = true;
+
+		xdiff = x - yrot;
+		ydiff = -y + xrot;
+	}
+	else
+		mouseDown = false;
+}
+
+void mouseMotion(int x, int y)
+{
+	if (mouseDown)
+	{
+		yrot = x - xdiff;
+		xrot = y + ydiff;
+
+		glutPostRedisplay();
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(500, 500);
+
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+	glutCreateWindow("limas segitiga");
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(specialKeyboard);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	glutReshapeFunc(resize);
+	//glutIdleFunc(idle);
+
+	if (!init())
+		return 1;
+
+	glutMainLoop();
+
+	return 0;
 }
